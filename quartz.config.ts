1baecf759cfc9b0b44a3029f5915dd887ce99290
Sungwoo Kang <r191d1ty.w00@gmail.com>
2025-09-18 04:05:39 +0900

    fix: Change base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "ingbunga.github.io",
+    baseUrl: "ingbunga.github.io/blog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
